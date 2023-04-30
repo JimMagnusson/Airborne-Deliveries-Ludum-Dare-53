f742c0f09d4a56e1001f3e1de22a434b8f1080d5
Jim Magnusson <jim.magnusson97@gmail.com>
2023-04-30 22:21:02 +0200

    Fix jump bug again

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -109,7 +109,7 @@ public class PlayerMovementController : MonoBehaviour
         if(rb.velocity.y < 0) {
             rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
         }
-        else if (rb.velocity.y > 0 && !Input.GetButton("Jump")) {
+        else if (rb.velocity.y > 0 && !(Input.GetButton("Jump") && !launched)) {
             rb.velocity +=  Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
         }
     }
